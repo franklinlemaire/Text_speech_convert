df7553283a4c5ad54172a17ea93ed83b3f3a59fd
franklinlemaire <42901183+franklinlemaire@users.noreply.github.com>
2024-07-09 15:37:04 +0100

    ajout d'une condition

--- texttoSpeech.py
+++ texttoSpeech.py
@@ -1,6 +1,17 @@
 from gtts import gTTS #Import du module de Text pour la parole
-text = "Bonjour les amis comment allez vous ?"
-language  = "fr" #choix de la langue
+
+language = input("Choisissez votre langue \n")
+text = input("Entrer votre texte à traduire \n")
+
+if (language=="en"):
+    obj = gTTS(text=text, lang=language, slow=False)
+elif(language=="fr"):
+    obj = gTTS(text=text, lang=language, slow=False)   
+obj.save("sample.mp3")
+
+
+#text = "Bonjour les amis comment allez vous ?"
+#language  = "fr" #choix de la langue
 
 obj = gTTS(text=text, lang=language, slow=False)
 obj.save("sample.mp3")
