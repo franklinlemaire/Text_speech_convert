795d09c81fe011e2e937e9002c667a8974419786
franklinlemaire <42901183+franklinlemaire@users.noreply.github.com>
2024-09-12 17:51:25 +0100

    Convertisseur de texte

--- texttoSpeech.py
+++ texttoSpeech.py
@@ -1,19 +1,10 @@
 from gtts import gTTS #Import du module de Text pour la parole
 
-language = input("Choisissez votre langue \n")
-text = input("Entrer votre texte à traduire \n")
+language = input('Choisissez votre langue ("fr" pour le français et "en" pour l\'anlais) \n')
+text = input("Entrer votre texte à transformer en fichier vocal \n")
 
 if (language=="en"):
     obj = gTTS(text=text, lang=language, slow=False)
 elif(language=="fr"):
     obj = gTTS(text=text, lang=language, slow=False)   
 obj.save("sample.mp3")
-
-
-#text = "Bonjour les amis comment allez vous ?"
-#language  = "fr" #choix de la langue
-
-obj = gTTS(text=text, lang=language, slow=False)
-obj.save("sample.mp3")
-
-
